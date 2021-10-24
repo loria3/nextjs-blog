22478840328d251b3780bc8ae65282699c2af9c5
loria3@gmail.com <loria33@gmail.com>
2021-10-23 23:59:43 -0400

    init

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8b5653f6d982459c18d4";
+/******/ 	var hotCurrentHash = "dc00751fdaa0e37afa2a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
